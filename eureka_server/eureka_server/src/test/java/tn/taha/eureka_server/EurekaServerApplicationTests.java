2ceb35eee599b382045a493d74530d6b3997e0f0
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-10-02 23:13:10 +0200

    eureka server

---
+++ eureka_server/eureka_server/src/test/java/tn/taha/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.taha.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
