7655efc3873ee3fba2d9fbc9d42cbfa7f98d0274
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-10-02 23:20:33 +0200

    staff service init

---
+++ ms_saff_management/src/main/java/tn/taha/ms_saff_management/MsSaffManagementApplication.java
@@ -0,0 +1,13 @@
+package tn.taha.ms_saff_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSaffManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSaffManagementApplication.class, args);
+	}
+
+}
---
+++ ms_saff_management/src/test/java/tn/taha/ms_saff_management/MsSaffManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.taha.ms_saff_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSaffManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
