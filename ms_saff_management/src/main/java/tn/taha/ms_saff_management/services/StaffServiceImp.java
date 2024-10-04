29b72b22059a0072fa84dcf10c7c4a85a7f70d90
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-10-04 21:29:41 +0200

    add staf and get by id

--- ms_saff_management/src/main/java/tn/taha/ms_saff_management/services/StaffServiceImp.java
+++ ms_saff_management/src/main/java/tn/taha/ms_saff_management/services/StaffServiceImp.java
@@ -1,4 +1,34 @@
 package tn.taha.ms_saff_management.services;
 
-public class StaffServiceImp {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tn.taha.ms_saff_management.entities.Staff;
+import tn.taha.ms_saff_management.repository.StaffRepository;
+import tn.taha.ms_saff_management.services.Interfaces.IStaffService;
+
+import java.util.Date;
+import java.util.List;
+@Service
+public class StaffServiceImp implements IStaffService {
+
+    @Autowired
+    private StaffRepository staffRepository;
+    @Override
+    public Staff addStaff(Staff staff) {
+
+        return this.staffRepository.save(staff);
+    }
+
+    @Override
+    public Staff GetStaff(int id) {
+        if (staffRepository.findById(id).isPresent()) {
+            Staff staff = staffRepository.findById(id).get();
+
+            return staff;
+        }else {
+            return null;
+        }
+
+    }
 }
