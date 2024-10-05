cde118fe6408b086185f28d4219b3e595daa2b69
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-10-05 20:08:18 +0200

    hello world im done

--- eureka_server/src/main/java/tn/taha/eureka_server/EurekaServerApplication.java
+++ eureka_server/src/main/java/tn/taha/eureka_server/EurekaServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaServer
+
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
