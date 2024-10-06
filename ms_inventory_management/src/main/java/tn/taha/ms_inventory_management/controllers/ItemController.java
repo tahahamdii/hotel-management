0317de5ac508275a138ebaafd9ef2fa42d6e8d29
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-10-06 20:06:26 +0200

    adding and updateing item

---
+++ ms_inventory_management/src/main/java/tn/taha/ms_inventory_management/controllers/ItemController.java
@@ -0,0 +1,39 @@
+package tn.taha.ms_inventory_management.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tn.taha.ms_inventory_management.entities.Item;
+import tn.taha.ms_inventory_management.services.ItemServiceImp;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping(value = "/api/item")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+public class ItemController {
+    @Autowired()
+    private ItemServiceImp IS;
+
+    @PostMapping()
+    public ResponseEntity<Item> add(@RequestBody() Item item){
+        return new ResponseEntity<>(IS.addItem(item), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Item> update(@Valid @RequestBody Item item, @PathVariable Long id){
+        try {
+            Optional<Item> existItem = IS.show_item(id);
+            item.setId(id);
+            IS.addItem(item);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
