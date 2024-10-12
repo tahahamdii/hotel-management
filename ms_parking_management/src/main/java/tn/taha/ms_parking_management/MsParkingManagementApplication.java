b01033d6ebecca6e1b554f762db81c4c953e9be9
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-10-12 07:29:26 +0200

    parking demo

---
+++ ms_parking_management/src/main/java/tn/taha/ms_parking_management/MsParkingManagementApplication.java
@@ -0,0 +1,13 @@
+package tn.taha.ms_parking_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsParkingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsParkingManagementApplication.class, args);
+	}
+
+}
---
+++ ms_parking_management/src/test/java/tn/taha/ms_parking_management/MsParkingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.taha.ms_parking_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsParkingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
